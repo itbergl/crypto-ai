6259b9985d53f88be9aa3919816c58c3e15db444
Kaiqi Liang <kaiqi.liang@outlook.com>
2023-04-25 15:08:11 +0800

    Add pandas and ta

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import ccxt
+import pandas as pd
+from ta import volatility
+
 exchange = ccxt.kraken()
 ohlc = exchange.fetch_ohlcv('BTC/AUD')
-for candle in ohlc:
-    print(candle)
+df = pd.DataFrame(ohlc, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+bb_indicator = volatility.BollingerBands(df['close'])
+df['moving_average'] = bb_indicator.bollinger_mavg()
+print(df)
