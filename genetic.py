3d0e23ad20d8d60100763b7627015ca42ee1ee9a
itbergl <tberg644@gmail.com>
2023-05-04 00:11:19 +0800

    improved efficienct and added proposal

--- genetic.py
+++ genetic.py
@@ -7,6 +7,50 @@ POPULATION = 500
 Gene = list[int, float, int, int, float, int, int, float, int, int, float, int]
 Population = list[Gene]
 
+
+'''
+Proposal for more general approach:
+
+	Solution is an OR chain [GENE1, GENE2, GENE3, GENE4, ...], where GENEX is 
+	an AND chain of [LIT, LIT, LIT, LIT, ...] and LIT is a function IND > c*IND.
+
+	We could have a max size for Solutions and max size for GENEs. 
+
+	class Solution:
+		--> property) genes: list[GENE]
+	
+	class GENE:
+		--> property) literals: list[Literal]
+	
+	class Literal:
+		--> property) A -> string
+		--> property) B -> string
+		--> properrty) C -> float
+
+		--> function) call(df_row) -> (df_row[A] > c*df_row[B])
+	
+	^ Just ideas they don't have to be classes.
+
+	We could implement a crossover / mutation in each.
+
+	Since there is a fixed size for each solution (max_gene_len * max_literal_len)
+	each could even be represented by an array.
+
+	 (A > b*C)	(D > e*f)	(G > h*I) ...
+	 (J > k*L)	(M > n*O)	(P > q*R) ...
+	 (S > t*U)	(V > w*X)	(Y > z*(AA))
+	 .
+	 .
+	 .
+
+	 Which could be expressed as a string 
+
+	Solution =  [A > b*C OR D > e*f OR G > h*I OR ...] AND [J > k*L OR M > n*O OR P > q*R OR ...] AND [S > t*U OR V > w*X OR Y > z*(AA) OR ...] AND ...
+
+	Evalution could be vectorised on this array (using numpy.vectorize) to be faster than O(n^2)
+
+
+'''
 def rand_trigger(indicators_and_candle_values: StrSeriesPairs):
 	'''
 	Randomly create an expression of `A > c * B` where `A` and `B` are indicator or candle values.
@@ -33,7 +77,7 @@ def find_median(value, values):
 	'''
 	If the value is `0` or `NAN` find another value that is not, otherwise return `0`.
 	'''
-	if value != 0 and np.isnan(value):
+	if value != 0 and not np.isnan(value):
 		return value
 	for val in values:
 		if val != 0 and not np.isnan(val):
@@ -46,7 +90,7 @@ def rand_indicator_or_candle_val(indicators_and_candle_values: StrSeriesPairs):
 	'''
 	return random.randrange(0, len(indicators_and_candle_values))
 
-def evaluate(df: pd.DataFrame, pool: Population, indicators_and_candle_values: StrSeriesPairs):
+def evaluate(df_rows: list, pool: Population, indicators_and_candle_values: StrSeriesPairs):
 	'''
 	Call the fitness evaluation for each gene in the pool and return the restuls in fitnesses.
 	Add up the total fitness sum of all the genes in the whole generation and find the best one to return.
@@ -56,7 +100,7 @@ def evaluate(df: pd.DataFrame, pool: Population, indicators_and_candle_values: S
 	fit_sum = 0.
 	fitnesses = [0. for _ in range(POPULATION)]
 	for i in range(POPULATION):
-		fitnesses[i] = fitness(df, pool[i], indicators_and_candle_values)
+		fitnesses[i] = fitness(df_rows, pool[i], indicators_and_candle_values)
 		fit_sum += fitnesses[i]
 		if fitnesses[i] > max_fit:
 			max_fit = fitnesses[i]
@@ -85,14 +129,14 @@ def get_indicator_and_candle_values_from_gene(gene: Gene, indicators_and_candle_
 		indicators_and_candle_values[l][0],
 	)
 	
-def fitness(df: pd.DataFrame, gene: Gene, indicators_and_candle_values: StrSeriesPairs) -> float:
+def fitness(df_rows: list, gene: Gene, indicators_and_candle_values: StrSeriesPairs) -> float:
 	'''
 	Calculate the amount of money at the end of all the trades starting with $100.
 	'''
 	amount = 100.
 	buy_trigger = False
 	a, b, c, d, e, f, g, h, i, j, k, l = get_indicator_and_candle_values_from_gene(gene, indicators_and_candle_values)
-	for _, row in df.iterrows():
+	for row in df_rows:
 		if not buy_trigger and not np.isnan(row[c]) and not np.isnan(row[f]) and row[a] > b * row[c] and row[d] > e * row[f]:
 			amount -= amount * 0.02
 			amount /= row['close']
--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 import pandas as pd
+from tqdm import tqdm
 from data import retrieve_data, list_indicators_and_candle_values, add_all_indicators
 from genetic import POPULATION, Population, rand_trigger, evaluate, selection, crossover, mutation, get_indicator_and_candle_values_from_gene
 
 MAX_ITER = 30
 
-# Allow printing the entire data frame
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+if __name__=='__main__':
 
-# Set up data frame
-df = retrieve_data()
-indicators_and_candle_values = list_indicators_and_candle_values(df)
-df = add_all_indicators(df, indicators_and_candle_values)
+	# Allow printing the entire data frame
+	pd.set_option('display.max_columns', None)
+	pd.set_option('display.max_rows', None)
 
-# Initialise gene pools
-pool: Population = [
-    [
-		*rand_trigger(indicators_and_candle_values),
-		*rand_trigger(indicators_and_candle_values),
-		*rand_trigger(indicators_and_candle_values),
-		*rand_trigger(indicators_and_candle_values),
+	# Set up data frame
+	df = retrieve_data()
+	indicators_and_candle_values = list_indicators_and_candle_values(df)
+	df = add_all_indicators(df, indicators_and_candle_values)
+	df_rows = [row for _, row in df.iterrows()]
+
+	# Initialise gene pools
+	pool: Population = [
+		[
+			*rand_trigger(indicators_and_candle_values),
+			*rand_trigger(indicators_and_candle_values),
+			*rand_trigger(indicators_and_candle_values),
+			*rand_trigger(indicators_and_candle_values),
+		]
+		for _ in range(POPULATION)
 	]
-    for _ in range(POPULATION)
-]
-next: Population = [[] for _ in range(POPULATION)]
+	next: Population = [[] for _ in range(POPULATION)]
 
-# Run genetic algorithm for some number of iterations
-for iter in range(MAX_ITER):
-	print(f'iteration {iter}')
-	max_pos, max_fit, fit_sum, fitnesses = evaluate(df, pool, indicators_and_candle_values)
-	# Preserve best gene and replicate it so it appears twice in the next generation
-	next[0] = pool[max_pos]
-	next[1] = pool[max_pos]
-	# Do crossover for the rest of genes and mutate a small amount of them randomly
-	for i in range(2, POPULATION - 1, 2):
-		crossover(pool[selection(fit_sum, fitnesses)], pool[selection(fit_sum, fitnesses)], i, next)
-	mutation(next, indicators_and_candle_values)
-	pool = next
+	# Run genetic algorithm for some number of iterations
+	for _ in tqdm(range(MAX_ITER), total=MAX_ITER):
+		
+		max_pos, max_fit, fit_sum, fitnesses = evaluate(df_rows, pool, indicators_and_candle_values)
+		# Preserve best gene and replicate it so it appears twice in the next generation
+		next[0] = pool[max_pos]
+		next[1] = pool[max_pos]
+		# Do crossover for the rest of genes and mutate a small amount of them randomly
+		for i in range(2, POPULATION - 1, 2):
+			crossover(pool[selection(fit_sum, fitnesses)], pool[selection(fit_sum, fitnesses)], i, next)
+		mutation(next, indicators_and_candle_values)
+		pool = next
 
-# Print out the best gene after all the evolution
-max_pos, max_fit, fit_sum, fitnesses = evaluate(df, pool, indicators_and_candle_values)
-print(f'best bot earns ${max_fit}')
-a, b, c, d, e, f, g, h, i, j, k, l = get_indicator_and_candle_values_from_gene(pool[max_pos], indicators_and_candle_values)
-print(f'buy trigger: {a} > {b} * {c} & {d} > {e} * {f}')
-print(f'sell trigger: {g} > {h} * {i} & {j} > {k} * {l}')
+	# Print out the best gene after all the evolution
+	max_pos, max_fit, fit_sum, fitnesses = evaluate(df_rows, pool, indicators_and_candle_values)
+	print(f'best bot earns ${max_fit}')
+	a, b, c, d, e, f, g, h, i, j, k, l = get_indicator_and_candle_values_from_gene(pool[max_pos], indicators_and_candle_values)
+	print(f'buy trigger: {a} > {b} * {c} & {d} > {e} * {f}')
+	print(f'sell trigger: {g} > {h} * {i} & {j} > {k} * {l}')
