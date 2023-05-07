27e70baaf91ce9a58f600e3168e42fe0204f40f3
itbergl <tberg644@gmail.com>
2023-05-07 19:47:50 +0800

    trialling a class-based approach

--- genetic.py
+++ genetic.py
@@ -1,137 +1,127 @@
 import random
-import pandas as pd
 import numpy as np
-from data import StrSeriesPairs
 
-POPULATION = 501 # Has to be an odd number
-Gene = list[int, float, int, int, float, int, int, float, int, int, float, int]
-Population = list[Gene]
+class Expression:
 
-def rand_trigger(indicators_and_candle_values: StrSeriesPairs):
-	'''
-	Randomly create an expression of `A > c * B` where `A` and `B` are indicator or candle values.
-	'''
-	lhs = rand_indicator_or_candle_val(indicators_and_candle_values)
-	rhs = rand_indicator_or_candle_val(indicators_and_candle_values)
-	while lhs == rhs:
-		rhs = rand_indicator_or_candle_val(indicators_and_candle_values)
-	c = rand_constant(lhs, rhs, indicators_and_candle_values)
-	return lhs, c, rhs
+	def __init__(self, df):
+		'''
+		Randomly create an expression of `A > c * B` where `A` and `B` are indicator or candle values.
+		'''
+		# choose two different random headers from df
+		self.lhs, self.rhs = random.sample(list(range(len(df.columns))), 2)
+		
+		# get c based on joint normal distribution of A and B
+		self.c = self.rand_constant(df)
 
-def rand_constant(lhs: int, rhs: int, indicators_and_candle_values: StrSeriesPairs) -> float:
-	'''
-	Randomly create a constant in the range of `- A / B` to `A / B`.
-	Calculate this range using the median value of `A` and `B`.
-	'''
-	values1 = indicators_and_candle_values[lhs][1]
-	values2 = indicators_and_candle_values[rhs][1]
-	val1 = find_median(values1[len(values1) / 2], values1)
-	val2 = find_median(values2[len(values2) / 2], values2)
-	return 0 if val2 == 0 else (random.random() - 0.5) * (val1 // val2)
+	def rand_constant(self, df, std = 1):
+		# A, B = df.iloc[0, self.lhs], df.iloc[0, self.rhs]
+		return np.random.normal(0, std, 1)[0]
+		# random float from -10,000 to +10,000
 
-def find_median(value, values):
-	'''
-	If the value is `0` or `NAN` find another value that is not, otherwise return `0`.
-	'''
-	if value != 0 and not np.isnan(value):
-		return value
-	for val in values:
-		if val != 0 and not np.isnan(val):
-			return val
-	return 0
 
-def rand_indicator_or_candle_val(indicators_and_candle_values: StrSeriesPairs):
-	'''
-	Randomly pick an indicator or candle value and return its index.
-	'''
-	return random.randrange(0, len(indicators_and_candle_values))
+		# neg = 0 if B == 0 else np.random.normal(-A/B or 0, std, 1)
+		# pos = 0 if B == 0 else np.random.normal(A/B or 0, std, 1)
+	
+		# return random.choice([neg, pos])
 
-def evaluate(df_rows: pd.Series, pool: Population, indicators_and_candle_values: StrSeriesPairs):
-	'''
-	Call the fitness evaluation for each gene in the pool and return the restuls in fitnesses.
-	Add up the total fitness sum of all the genes in the whole generation and find the best one to return.
-	'''
-	max_pos = 0
-	max_fit = 0.
-	fit_sum = 0.
-	fitnesses = [0. for _ in range(POPULATION)]
-	for i in range(POPULATION):
-		fitnesses[i] = fitness(df_rows, pool[i], indicators_and_candle_values)
-		fit_sum += fitnesses[i]
-		if fitnesses[i] > max_fit:
-			max_fit = fitnesses[i]
-			max_pos = i
-	return max_pos, max_fit, fit_sum, fitnesses
+	def evaluate(self, df_row):
+		return df_row[self.lhs] > self.c * df_row[self.rhs]
 
-def get_indicator_and_candle_values_from_gene(gene: Gene, indicators_and_candle_values: StrSeriesPairs):
-	'''
-	Return the buy and sell triggers in the expression of:
-	`buy_trigger = a > b * c and d > e * f`,
-	`sell_trigger = g > h * i and j > k * l`
-	'''
-	a, b, c, d, e, f, g, h, i, j, k, l = gene
-	return (
-		indicators_and_candle_values[a][0],
-		b,
-		indicators_and_candle_values[c][0],
-		indicators_and_candle_values[d][0],
-		e,
-		indicators_and_candle_values[f][0],
-		indicators_and_candle_values[g][0],
-		h,
-		indicators_and_candle_values[i][0],
-		indicators_and_candle_values[j][0],
-		k,
-		indicators_and_candle_values[l][0],
-	)
+	def to_string(self, df):
+		return f'{df.columns[self.lhs]} > {self.c} * {df.columns[self.rhs]}'
+	
+class Gene:
 
-def fitness(df_rows: pd.Series, gene: Gene, indicators_and_candle_values: StrSeriesPairs) -> float:
-	'''
-	Calculate the amount of money at the end of all the trades starting with $100.
-	'''
-	amount = 100.
-	buy_trigger = False
-	a, b, c, d, e, f, g, h, i, j, k, l = get_indicator_and_candle_values_from_gene(gene, indicators_and_candle_values)
-	for row in df_rows:
-		if not buy_trigger and not np.isnan(row[c]) and not np.isnan(row[f]) and row[a] > b * row[c] and row[d] > e * row[f]:
-			amount -= amount * 0.02
-			amount /= row['close']
-			buy_trigger = True
-		elif buy_trigger and not np.isnan(row[i]) and not np.isnan(row[l]) and row[g] > h * row[i] and row[j] > k * row[l]:
-			amount *= row['close'] * (1 - 0.02)
-			buy_trigger = False
-	return amount
+	def __init__(self, df, n_expressions=2):
+		self.buy_trigger = [Expression(df) for _ in range(n_expressions)]
+		self.sell_trigger = [Expression(df) for _ in range(n_expressions)]
 
-def selection(fit_sum: float, fitnesses: list[float]):
-	'''
-	Implement wheel of fitness to select a random gene biased to reflect its fitness.
-	Wheel segments are covered with gene symbols, where the segment size matches the relative fitness level (twice the fitness means twice the area).
-	Assuming each spin is random, it will select a random gene with the desired bias according to their fitness levels.
-	'''
-	wheel = random.randrange(0, round(fit_sum))
-	i = 0
-	count = fitnesses[0]
-	while count < wheel:
-		i += 1
-		count += fitnesses[i]
-	return i
+	def evaluate_buys(self, df_row):
+		return all(exp.evaluate(df_row) for exp in self.buy_trigger)
+	
+	def evaluate_sells(self, df_row):
+		return all(exp.evaluate(df_row) for exp in self.sell_trigger)
 
-def crossover(g1: Gene, g2: Gene, pos: int, next_gen: Population):
-	'''
-	Given `2` genes `g1` and `g2` which were selected through the selection process.
-	Randomly pick a cut out of the `4` expressions.
-	Then the left side of the cut of `g1` is glued to the right side of the cut of `g2` and vice versa.
-	These two new genes will enter the next generation of the process.
- 	'''
-	cut = random.choice((3, 6, 9))
-	next_gen[pos] = [*[g1[i] for i in range(cut)], *[g2[i] for i in range(cut, len(g1))]]
-	next_gen[pos + 1] = [*[g2[i] for i in range(cut)], *[g1[i] for i in range(cut, len(g1))]]
+	def to_string(self, df):
+		return f'buy: {" & ".join(exp.to_string(df) for exp in self.buy_trigger)}\nsell: {" & ".join(exp.to_string(df) for exp in self.sell_trigger)}'
+	
+class Population:
 
-def mutation(pool: Population, indicators_and_candle_values: StrSeriesPairs):
-	'''
-	Randomly change one of the constant values in each of the `5` genes picked randomly from the pool.
- 	'''
-	for _ in range(5):
-		i = random.randrange(0, POPULATION)
-		j = random.choice((1, 4, 7, 10))
-		pool[i][j] = rand_constant(pool[i][j - 1], pool[i][j + 1], indicators_and_candle_values)
+	def __init__(self, df, population_size=501):
+		self.df = df
+		self.df_rows = [row for _, row in df.iterrows()]
+		self.pool = [Gene(df) for _ in range(population_size)]
+
+
+	def evaluate(self):
+		'''
+		Call the fitness evaluation for each gene in the pool and return the restuls in fitnesses.
+		Add up the total fitness sum of all the genes in the whole generation and find the best one to return.
+		'''
+		fitnesses = [self.fitness(gene) for gene in self.pool]
+		fit_sum = sum(fitnesses)
+		max_pos = np.argmax(fitnesses)
+		max_fit = fitnesses[max_pos]
+
+		return max_pos, max_fit, fit_sum, fitnesses
+
+
+	def fitness(self, gene: Gene) -> float:
+		'''
+		Calculate the amount of money at the end of all the trades starting with $100.
+		'''
+		amount = 100.
+		buy_trigger = False
+
+		for row in self.df_rows:
+			if not buy_trigger:
+				if gene.evaluate_buys(row):
+					amount -= amount * 0.02
+					amount /= row['close']
+					buy_trigger = True
+
+			elif buy_trigger:
+				if gene.evaluate_sells(row):
+					amount *= row['close'] * (1 - 0.02)
+					buy_trigger = False
+		return amount
+
+	def selection(self, fit_sum: float, fitnesses: list[float]):
+		'''
+		Implement wheel of fitness to select a random gene biased to reflect its fitness.
+		Wheel segments are covered with gene symbols, where the segment size matches the relative fitness level (twice the fitness means twice the area).
+		Assuming each spin is random, it will select a random gene with the desired bias according to their fitness levels.
+		'''
+		wheel = random.randrange(0, round(fit_sum))
+		i = 0
+		count = fitnesses[0]
+		while count < wheel:
+			i += 1
+			count += fitnesses[i]
+		return i
+
+	def crossover(self, fitsum: float, fitnesses: list[float]):
+		'''
+		Given `2` genes `g1` and `g2` which were selected through the selection process.
+		Randomly pick a cut out of the `4` expressions.
+		Then the left side of the cut of `g1` is glued to the right side of the cut of `g2` and vice versa.
+		These two new genes will enter the next generation of the process.
+		'''
+		for i in range(1, len(self.pool), 2):
+			a = self.selection(fitsum, fitnesses)
+			b = self.selection(fitsum, fitnesses)
+
+			geneA, geneB = self.pool[a], self.pool[b]
+			for triggerA, triggerB in [(geneA.buy_trigger, geneB.buy_trigger), (geneA.sell_trigger, geneB.sell_trigger)]:
+				cut = random.randrange(1, len(triggerA))
+				triggerA[:cut], triggerB[cut:] = triggerB[cut:], triggerA[:cut]
+			
+
+	def mutate(self, N = 5):
+		'''
+		Randomly change one of the constant values in each of the `N` genes picked randomly from the pool.
+		'''
+		for gene in random.sample(self.pool, N):
+			trigger = gene.buy_trigger if random.choice([True, False]) else gene.sell_trigger
+			exp = random.choice(trigger)
+			exp.c = exp.rand_constant(self.df)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from tqdm import tqdm
 from data import retrieve_data, list_indicators_and_candle_values, add_all_indicators, save_data
-from genetic import POPULATION, Population, rand_trigger, evaluate, selection, crossover, mutation, get_indicator_and_candle_values_from_gene
+from genetic import Population
 
 MAX_ITER = 30
 
@@ -10,48 +10,38 @@ if __name__=='__main__':
 	pd.set_option('display.max_columns', None)
 	pd.set_option('display.max_rows', None)
 
-	# Set up data frame
+	# Set up dataframe
 	df = retrieve_data()
 	indicators_and_candle_values = list_indicators_and_candle_values(df)
 	df = add_all_indicators(df, indicators_and_candle_values)
-	df_rows = [row for _, row in df.iterrows()]
+	df.to_csv('data.csv')
 
 	# Initialise gene pools
-	pool: Population = [
-		[
-			*rand_trigger(indicators_and_candle_values),
-			*rand_trigger(indicators_and_candle_values),
-			*rand_trigger(indicators_and_candle_values),
-			*rand_trigger(indicators_and_candle_values),
-		]
-		for _ in range(POPULATION)
-	]
-	next_gen: Population = [[] for _ in range(POPULATION)]
+	pool = Population(df)
 
 	# record bot values for visualisation
 	bot_record = []
 
 	# Run genetic algorithm for some number of iterations
 	for _ in tqdm(range(MAX_ITER), total=MAX_ITER):
-		max_pos, max_fit, fit_sum, fitnesses = evaluate(df_rows, pool, indicators_and_candle_values)
+		max_pos, max_fit, fit_sum, fitnesses = pool.evaluate()
 
 		# append the value record of the best bot
 		item = {'max_pos': max_pos, 'max_fit': max_fit, 'fit_sum': fit_sum, 'fitnesses': fitnesses}
 		bot_record.append(item)
 
 		# Preserve best gene and replicate it so it appears twice in the next generation
-		next_gen[0] = pool[max_pos]
+		pool.pool[0] = pool.pool[max_pos]
 		# Do crossover for the rest of genes and mutate a small amount of them randomly
-		for i in range(1, POPULATION, 2):
-			crossover(pool[selection(fit_sum, fitnesses)], pool[selection(fit_sum, fitnesses)], i, next_gen)
-		mutation(next_gen, indicators_and_candle_values)
-		pool = next_gen
+
+		pool.crossover(fit_sum, fitnesses)
+		pool.mutate()
 
 	# Print out the best gene after all the evolution
-	max_pos, max_fit, fit_sum, fitnesses = evaluate(df_rows, pool, indicators_and_candle_values)
+	max_pos, max_fit, fit_sum, fitnesses = pool.evaluate()
 	print(f'best bot earns ${max_fit:.5f}')
-	a, b, c, d, e, f, g, h, i, j, k, l = get_indicator_and_candle_values_from_gene(pool[max_pos], indicators_and_candle_values)
-	print(f'buy trigger: {a} > {b:.5f} * {c} & {d} > {e:.5f} * {f}')
-	print(f'sell trigger: {g} > {h:.5f} * {i} & {j} > {k:.5f} * {l}')
 
+	best_gene = pool.pool[max_pos]
+
+	print(best_gene.to_string(df))
 	save_data('bot_record.csv', bot_record)
